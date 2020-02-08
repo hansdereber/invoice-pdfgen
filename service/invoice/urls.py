751c91a60baabb499b1d72c28373054858a600d9
Christian <christian.geisler@posteo.de>
2020-02-08 18:29:43 +0100

    add proxy config and change service base url

--- service/invoice/urls.py
+++ service/invoice/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('pdfgen/', include('pdfgen.urls')),
+    path('api/', include('pdfgen.urls')),
     path('admin/', admin.site.urls),
 ]
