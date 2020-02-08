b7a86f310025f995f7b7998e1fac1188473126e1
Christian <christian.geisler@posteo.de>
2020-02-08 12:55:30 +0100

    add new layout components and restructure directories

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,17 +3,19 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
-import {ToolbarComponent} from './toolbar/toolbar.component';
+import {ToolbarComponent} from './layout/toolbar/toolbar.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import {MatExpansionModule} from "@angular/material/expansion";
-import {HeaderComponent} from './header/header.component';
-import {FooterComponent} from './footer/footer.component';
-import {BodyComponent} from './body/body.component';
-import {ExpensesComponent} from './expenses/expenses.component';
+import {HeaderComponent} from './forms/header/header.component';
+import {FooterComponent} from './forms/footer/footer.component';
+import {BodyComponent} from './forms/body/body.component';
+import {ExpensesComponent} from './forms/expenses/expenses.component';
 import {MatSelectModule} from "@angular/material/select";
 import {MatInputModule} from "@angular/material/input";
 import {MatFormFieldModule} from "@angular/material/form-field";
+import { ContentComponent } from './layout/content/content.component';
+import { LayoutFooterComponent } from './layout/layout-footer/layout-footer.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +24,9 @@ import {MatFormFieldModule} from "@angular/material/form-field";
     HeaderComponent,
     FooterComponent,
     BodyComponent,
-    ExpensesComponent
+    ExpensesComponent,
+    ContentComponent,
+    LayoutFooterComponent
   ],
   imports: [
     BrowserModule,
